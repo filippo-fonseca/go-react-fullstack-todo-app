918b0dd02d608d1f545ef0a718c659616c225985
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 20:49:47 -0600

    initial create atodo

--- main.go
+++ main.go
@@ -7,12 +7,38 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Hello, sssssWorsld!")
 	app := fiber.New()
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{ "msg": "Hello, World!" })
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{} // { id: 0, completed: false, body: "" }
+		
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{ "msg": "Todo body is required" })
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
