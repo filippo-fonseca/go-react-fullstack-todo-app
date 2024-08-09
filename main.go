852bc59c7f9229f911f4373a160193580cff8bfc
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 21:24:20 -0600

    patch stuff

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 		return c.Status(200).JSON(fiber.Map{ "msg": "Hello, World!" })
 	})
 
+	//create a todo
 	app.Post("/api/todos", func(c *fiber.Ctx) error {
 		todo := &Todo{} // { id: 0, completed: false, body: "" }
 		
@@ -38,6 +39,21 @@ func main() {
 		todos = append(todos, *todo)
 
 		return c.Status(201).JSON(todo)
+
+	})
+
+	//update a todo
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id := c.Params("id")
+
+		for i, todo := range todos {
+			if fmt.Sprint(todo.ID) == id {
+				todos[i].Completed = true
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{ "msg": "Todo not found" })
 
 	})
 
