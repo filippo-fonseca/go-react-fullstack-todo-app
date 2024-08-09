cad05827857483c8c30fc88a91d770533a625cef
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 12:14:30 -0600

    initial real-time db integration to react

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,31 +1,32 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+export type Todo = {
+    _id: number;
+    body: string;
+    completed: boolean;
+};
 
 const TodoList = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const todos = [
-        {
-            _id: 1,
-            body: "Buy groceries",
-            completed: true,
+    const { data: todos, isLoading } = useQuery<Todo[]>({
+        queryKey: ["todos"],
+        queryFn: async () => {
+            try {
+                const res = await fetch("http://localhost:4000/api/todos");
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong!");
+                }
+
+                return data || [];
+            } catch (error) {
+                console.error(error);
+            }
         },
-        {
-            _id: 2,
-            body: "Walk the dog",
-            completed: false,
-        },
-        {
-            _id: 3,
-            body: "Do laundry",
-            completed: false,
-        },
-        {
-            _id: 4,
-            body: "Cook dinner",
-            completed: true,
-        },
-    ];
+    });
+
     return (
         <>
             <Text
