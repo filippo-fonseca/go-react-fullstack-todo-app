94070dc8badacc1990cec7dc577f2c2adb0b770b
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 22:18:10 -0600

    delete todos

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func main() {
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
-	// app.Delete("/api/todos/:id", deleteTodo)
+	app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
 
@@ -133,6 +133,23 @@ func updateTodo(c *fiber.Ctx) error {
 
 }
 
-// func deleteTodo(c *fiber.Ctx) error {
+func deleteTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-// }
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{ "error": "Invalid todo ID" })
+	}
+
+	filter := bson.M{"_id": objectID }
+
+	_, err = collection.DeleteOne(context.Background(), filter)
+
+	if err != nil {
+	  return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{ "success": "Todo deleted successfully" })
+
+
+}
