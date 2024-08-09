596a5f026b30d1d6780915e3ddc1882bd2cb014a
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 11:57:04 -0600

    add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import { Button } from '@chakra-ui/react';
+import { Container, Stack } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Stack h='100vh'>
+      <Navbar />
+      <Container>
+        {/* <TodoForm />
+        <TodoList /> */}
+      </Container>
+    </Stack>
   );
 }
 
