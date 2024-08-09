0e4a7edd9e03ec6348b143a899058dd5c3c0849a
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 22:14:05 -0600

    proper update todo

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
-	// app.Patch("/api/todos/:id", updateTodo)
+	app.Patch("/api/todos/:id", updateTodo)
 	// app.Delete("/api/todos/:id", deleteTodo)
 
 	port := os.Getenv("PORT")
@@ -112,9 +112,26 @@ func createTodo(c *fiber.Ctx) error {
 	return c.Status(201).JSON(todo)
 }
 
-// func updateTodo(c *fiber.Ctx) error {
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
 
-// }
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{ "error": "Invalid todo ID" })
+	}
+
+	filter := bson.M{ "_id": objectID }
+	update := bson.M{ "$set": bson.M{ "completed": true } }
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{ "success": "Todo updated successfully" })
+
+}
 
 // func deleteTodo(c *fiber.Ctx) error {
 
