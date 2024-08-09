48d58f48a47eb7bc4d174360a6f1c8d8f89bbf3a
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 13:23:28 -0600

    nicer light theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import theme from "./chakra/theme.ts";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <ChakraProvider>
+            <ChakraProvider theme={theme}>
                 <App />
             </ChakraProvider>
         </QueryClientProvider>
