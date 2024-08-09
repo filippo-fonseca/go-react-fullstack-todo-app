01c568898fe2d157e9b43f907b24ed1af914aab6
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-08 20:40:55 -0600

    add proper log

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Println("Hello, sssssWorsld!")
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{ "msg": "Hello, World!" })
+	})
+
 	log.Fatal(app.Listen(":4000"))
 }
