92a42674502a9aa6fdd6dfc44a024ca736deb649
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 12:01:15 -0600

    proper components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Container, Stack } from '@chakra-ui/react';
 import Navbar from './components/Navbar';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <Container>
         <TodoForm />
-        {/* <TodoList /> */}
+        <TodoList />
       </Container>
     </Stack>
   );
