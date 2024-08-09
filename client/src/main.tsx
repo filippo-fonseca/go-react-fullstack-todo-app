332c49db00336bf5447cfec51846f5b35330275e
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 12:05:58 -0600

    install tanstack

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </StrictMode>
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <ChakraProvider>
+                <App />
+            </ChakraProvider>
+        </QueryClientProvider>
+    </StrictMode>
 );
