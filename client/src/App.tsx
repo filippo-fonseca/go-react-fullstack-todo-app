fc843bf69f2a9d19686375d3ae998e81a3fe2d4d
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-09 12:03:44 -0600

    proper prettier and gradient text stuff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
-import { Container, Stack } from '@chakra-ui/react';
-import Navbar from './components/Navbar';
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return (
-    <Stack h='100vh'>
-      <Navbar />
-      <Container>
-        <TodoForm />
-        <TodoList />
-      </Container>
-    </Stack>
-  );
+    return (
+        <Stack h="100vh">
+            <Navbar />
+            <Container>
+                <TodoForm />
+                <TodoList />
+            </Container>
+        </Stack>
+    );
 }
 
 export default App;
